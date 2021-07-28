5a6f1c310c603888e32ad51eaed1745c4723b000
pedrobeltraoperalva@gmail.com <pedrobeltraoperalva@gmail.com>
2021-07-28 14:56:06 -0300

    mudança na rota do app listen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
 //executa as requisição vindas da porta 3032
-app.listen(3032, () => {
-    console.log(`Server is running at http://localhost:3032`)
+app.listen(process.env.PORT || 3032, () => {
+    console.log(`Server is running at http://localhost:${process.env.PORT || 3032}`)
 })
