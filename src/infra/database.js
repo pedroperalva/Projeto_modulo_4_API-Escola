849672ecfefa9bf64df0403366733cc8c06b30c6
pedrobeltraoperalva@gmail.com <pedrobeltraoperalva@gmail.com>
2021-08-23 14:55:04 -0300

    teste

--- src/infra/database.js
+++ src/infra/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect:'sqlite',
-    storage:'../projeto_modulo4/src/infra/database.sqlite'
+    storage:'src/infra/database.sqlite'
 })
 try {
     sequelize.authenticate();
