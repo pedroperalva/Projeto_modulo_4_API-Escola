eb662cd05b0f9102e05fe85c6b4999965b51483c
SamStarlling <samelabarbosa.06@gmail.com>
2021-08-24 15:20:03 -0300

    adicionado o cors ao projeto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 //requires
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 
 //middlewares
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
+app.use(cors())
 
 
 //importar o controller
